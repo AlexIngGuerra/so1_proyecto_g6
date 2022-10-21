e526a0281875ecdd5e55c8f87805226a8baad8bf
gkruiz <golwerruiz10@gmail.com>
2022-10-20 21:10:04 -0600

    producer

--- goREST/main.go
+++ goREST/main.go
@@ -5,8 +5,11 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
@@ -21,10 +24,18 @@ const (
 	brokerAddress = "localhost:9092"
 )
 
+type Informacion struct {
+	team1 string
+	team2 string
+	score string
+	phase string
+}
+
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/getinfo", getDataFiltro)
+	mux.HandleFunc("/getinfo", guardaInfo)
+	mux.HandleFunc("/guardainfo", guardaInfo)
 
 	log.Printf("listening on port %s", *flagPort)
 	log.Fatal(http.ListenAndServe(":"+*flagPort, mux))
@@ -52,6 +63,58 @@ func getDataFiltro(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprint(w, string(info))
 
+}
+
+func guardaInfo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body",
+			http.StatusInternalServerError)
+	} else {
+		cuerpo := string(body)
+
+		if (len(cuerpo)) > 0 {
+
+			ctx := context.Background()
+			go produce(ctx, cuerpo)
+			log.Printf("data: %s", (cuerpo))
+			fmt.Fprint(w, cuerpo)
+		}
+
+	}
+
+	log.Printf("data: %s", "ingreso en data nueva")
+
+}
+
+func produce(ctx context.Context, data string) {
+	// initialize a counter
+	i := 0
+
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{brokerAddress},
+		Topic:   topic,
+	})
+
+	//for {
+	err := w.WriteMessages(ctx, kafka.Message{
+		Key: []byte(strconv.Itoa(i)),
+		// create an arbitrary message payload for the value
+		Value: []byte(data),
+	})
+	if err != nil {
+		panic("no guardo mensaje" + err.Error())
+	}
+
+	// log a confirmation once the message is written
+	fmt.Println("writes:", i)
+	i++
+	// sleep for a second
+	time.Sleep(time.Second)
+	//}
 }
 
 func consume(ctx context.Context) string {
